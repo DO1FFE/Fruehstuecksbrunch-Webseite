2e1ea18809c7777f3e1065a7c55c88a83f7803d6
Erik Schauer <do1ffe@darc.de>
2024-01-21 16:35:18 +0100

    Update brunch.py

--- brunch.py
+++ brunch.py
@@ -353,6 +353,7 @@ def index():
             <div class="container mx-auto px-4">
                 <h1 class="text-3xl font-bold text-center my-6">L11 Frühstücksbrunch Anmeldung - Sonntag, {{ next_brunch_date_str }} 10 Uhr</h1>
                 <h2 class="text-xl font-bold text-center my-6">Teilnehmende Personen (ohne Kaffeetrinker): {{ total_participants_excluding_coffee_only }}, Kaffeetrinker: {{ coffee_only_participants }}</h2>
+                <p class="text-red-500 text-center">Hinweis: Die Anmeldung ist ab Freitag 0 Uhr vor dem Brunch geschlossen und wird am Brunch-Sonntag um 15 Uhr wieder geöffnet.</p>
                 <p class="text-red-500">{{ error_message }}</p>
                 <form method="post" class="mb-4">
                     <table>
