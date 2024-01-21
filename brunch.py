44b3b466c09203ff15f0f5ad0d9483194de4e5b3
Erik Schauer <do1ffe@darc.de>
2024-01-21 16:10:21 +0100

    Update brunch.py

--- brunch.py
+++ brunch.py
@@ -217,11 +217,12 @@ def is_registration_open():
     next_brunch = berlin_tz.localize(datetime.strptime(next_brunch_date(), '%d.%m.%Y'))
     friday_before_brunch = next_brunch - timedelta(days=2)
 
-    # Stellen Sie sicher, dass friday_before_brunch und next_brunch auf Mitternacht bzw. 15 Uhr gesetzt sind
+    # Stellen Sie sicher, dass friday_before_brunch auf Mitternacht und next_brunch auf 15 Uhr gesetzt sind
     friday_before_brunch = friday_before_brunch.replace(hour=0, minute=0, second=0, microsecond=0)
     brunch_end_time = next_brunch.replace(hour=15, minute=0, second=0, microsecond=0)
 
-    return now < friday_before_brunch or now > brunch_end_time
+    # Die Registrierung ist geschlossen zwischen Freitag 0 Uhr und Sonntag 15 Uhr
+    return not (friday_before_brunch <= now <= brunch_end_time)
 
 def validate_name_or_call(text):
     """
